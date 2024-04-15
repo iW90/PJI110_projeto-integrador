42264031a03d933705afe43ec69c3c591abec119
InGrid <indynha.w@gmail.com>
2024-04-14 21:18:46 -0300

    feat: change style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const	Footer = () => {
 	return (
-		<div>
+		<>
 			<p>Projeto Integrador I</p>
 			<p>Univesp 2024</p>
-			<p>MIT Licence</p>
-		</div>
+			<p>© MIT Licence</p>
+		</>
 	);
 }
 
